36f3e56a70f70078c6dfad6952c56d6b75ab478f
Constanza Mardones <coty.tiff@gmail.com>
2021-02-18 15:28:03 -0300

    add ransack and kaminari, tweet controller

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ gem 'pg', '>= 0.18', '< 2.0'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 gem 'devise'
+gem 'ransack'
+gem 'kaminari', :git => 'https://github.com/kaminari/kaminari'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,9 @@
 class TweetsController < ApplicationController
+    before_action :set_book, only: %i[ show edit update destroy ]
+end
     def index
+        @q = Tweet.ransack(params[:q])
+        @tweet = @q.result(distinct: true).order(:asc).page(params[:page]).per(50)
      
     end
   
@@ -8,21 +12,55 @@ class TweetsController < ApplicationController
     end
   
     def new
+        @tweet = Tweet.new
     end
   
-    def creates
-      
+    def create
+        @tweet = Tweet.new(tweet_params)
+    
+        respond_to do |format|
+            if @tweet.save
+                format.html { redirect_to @tweet, notice: "El tweet fue creado" }
+                format.json { render :show, status: :created, location: @tweet }
+            else
+                format.html { render :new, status: :unprocessable_entity }
+                format.json { render json: @tweet.errors, status: :unprocessable_entity }
+            end
+        end
     end
   
     def edit
     end
   
     def update
+        respond_to do |format|
+            if @tweet.update(tweet_params)
+              format.html { redirect_to @tweet, notice: "los datos del libro fueron modificados" }
+              format.json { render :show, status: :ok, location: @tweet }
+            else
+              format.html { render :edit, status: :unprocessable_entity }
+              format.json { render json: @tweet.errors, status: :unprocessable_entity }
+            end
+          end
     end
   
     def destroy
-      
+        @tweet.destroy
+        respond_to do |format|
+          format.html { redirect_to books_url, notice: "El tweet fue destruido" }
+          format.json { head :no_content }
+        end
     end
-    
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_tweet
+      @tweet = Tweet.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def tweet_params
+      params.require(:tweet).permit(:content, :id_user)
+    end
   end
   
