3274b2e0aa5f14cf5143c33ce1ac1eb60cad1ac5
Constanza Mardones <coty.tiff@gmail.com>
2021-02-18 16:09:11 -0300

    add controllers tweets and views

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,7 @@
 class TweetsController < ApplicationController
-    before_action :set_book, only: %i[ show edit update destroy ]
-end
+    before_action :authenticate_user!, :except => [:index]
+    before_action :set_book, only: %i[ show destroy ]
+
     def index
         @q = Tweet.ransack(params[:q])
         @tweet = @q.result(distinct: true).order(:asc).page(params[:page]).per(50)
@@ -17,40 +18,42 @@ end
   
     def create
         @tweet = Tweet.new(tweet_params)
-    
-        respond_to do |format|
-            if @tweet.save
-                format.html { redirect_to @tweet, notice: "El tweet fue creado" }
-                format.json { render :show, status: :created, location: @tweet }
-            else
-                format.html { render :new, status: :unprocessable_entity }
-                format.json { render json: @tweet.errors, status: :unprocessable_entity }
-            end
+        if @tweet.save
+          flash[:success] = "Se creo exitosamente el Tweet"
+          redirect_to @tweet
+        else
+          flash[:error] = "Algo paso, intentalo de nuevo"
+          render 'new'
         end
     end
+
   
-    def edit
-    end
+    # def edit
+    # end
   
-    def update
-        respond_to do |format|
-            if @tweet.update(tweet_params)
-              format.html { redirect_to @tweet, notice: "los datos del libro fueron modificados" }
-              format.json { render :show, status: :ok, location: @tweet }
-            else
-              format.html { render :edit, status: :unprocessable_entity }
-              format.json { render json: @tweet.errors, status: :unprocessable_entity }
-            end
-          end
-    end
+    # def update
+    #     respond_to do |format|
+    #         if @tweet.update(tweet_params)
+    #           format.html { redirect_to @tweet, notice: "el tweet fue modificado" }
+    #           format.json { render :show, status: :ok, location: @tweet }
+    #         else
+    #           format.html { render :edit, status: :unprocessable_entity }
+    #           format.json { render json: @tweet.errors, status: :unprocessable_entity }
+    #         end
+    #       end
+    # end
   
     def destroy
-        @tweet.destroy
-        respond_to do |format|
-          format.html { redirect_to books_url, notice: "El tweet fue destruido" }
-          format.json { head :no_content }
+        @tweet = Tweet.find(params[:id])
+        if @tweet.destroy
+            flash[:success] = 'El tweet fue borrado con exito.'
+            redirect_to tweets_url
+        else
+            flash[:error] = 'algo paso, intentalo de nuevo'
+            redirect_to tweets_url
         end
     end
+    
 
     private
     # Use callbacks to share common setup or constraints between actions.
@@ -62,5 +65,5 @@ end
     def tweet_params
       params.require(:tweet).permit(:content, :id_user)
     end
-  end
+end
   
