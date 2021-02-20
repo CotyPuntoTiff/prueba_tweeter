b9d35867a2611c46a4914ccffcd080498611ae64
Constanza Mardones <coty.tiff@gmail.com>
2021-02-20 00:16:34 -0300

    add recursive reference

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_20_005759) do
+ActiveRecord::Schema.define(version: 2021_02_20_031527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,10 +27,11 @@ ActiveRecord::Schema.define(version: 2021_02_20_005759) do
 
   create_table "tweets", force: :cascade do |t|
     t.string "contents"
-    t.integer "retweet"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.bigint "tweet_id"
+    t.index ["tweet_id"], name: "index_tweets_on_tweet_id"
     t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
@@ -50,5 +51,6 @@ ActiveRecord::Schema.define(version: 2021_02_20_005759) do
 
   add_foreign_key "likes", "tweets"
   add_foreign_key "likes", "users"
+  add_foreign_key "tweets", "tweets"
   add_foreign_key "tweets", "users"
 end
