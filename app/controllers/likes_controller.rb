7f680ccc2f12f05820537a0d9eed346b3e19935f
Constanza Mardones <coty.tiff@gmail.com>
2021-02-19 23:41:34 -0300

    Like works

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,55 @@
+class LikesController < ApplicationController
+    before_action :find_tweet
+    before_action :find_like, only: [:destroy]
+    def index
+
+    end
+  
+    def show
+  
+    end
+  
+    def new
+    end
+  
+    def create
+      if already_liked?
+        flash[:notice] = "Puedes dar like solo una vez"
+      else
+        @tweet.likes.create(user_id: current_user.id)
+      end
+      redirect_to root_path(@tweet)
+    end
+  
+    def edit
+    end
+  
+    def update
+    end
+  
+    def destroy
+      if !(already_liked?)
+        flash[:notice] = "No puedes sacar el like"
+      else
+        @like.destroy
+      end
+      redirect_to root_path(@tweet)
+    end
+
+    private 
+    def find_tweet
+      @tweet = Tweet.find(params[:tweet_id])
+      
+    end
+
+    def find_like
+      @like = @tweet.likes.find(params[:id])
+   end
+   
+    def already_liked?
+      Like.where(user_id: current_user.id, tweet_id:
+      params[:tweet_id]).exists?
+    end
+    
+  end
+  
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_18_162734) do
+ActiveRecord::Schema.define(version: 2021_02_20_005759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,9 @@ ActiveRecord::Schema.define(version: 2021_02_18_162734) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "tweet_id"
+    t.bigint "user_id"
     t.index ["tweet_id"], name: "index_likes_on_tweet_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "tweets", force: :cascade do |t|
@@ -47,5 +49,6 @@ ActiveRecord::Schema.define(version: 2021_02_18_162734) do
   end
 
   add_foreign_key "likes", "tweets"
+  add_foreign_key "likes", "users"
   add_foreign_key "tweets", "users"
 end
